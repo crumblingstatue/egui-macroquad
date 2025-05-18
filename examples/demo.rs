9843b61cb4e50d5f4a8fdaa08cd5f0502efd061b
crumblingstatue <radiantstatue@gmail.com>
2025-05-18 16:44:17 +0200

    Edition 2024, dependency update

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,4 +1,4 @@
-use {egui_macroquad::egui, egui_macroquad::EguiMqInteg, macroquad::prelude::*};
+use {egui_macroquad::EguiMqInteg, egui_macroquad::egui, macroquad::prelude::*};
 
 fn window_conf() -> Conf {
     Conf {
